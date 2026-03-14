6ce5c299a75e48f3564ec545f856bf1a3f8cab94
endingofourlife <endingofourlife@gmail.com>
2026-03-14 18:02:28 +0200

    add validation messages

--- src/features/verbs/schemas.ts
+++ src/features/verbs/schemas.ts
@@ -4,5 +4,5 @@ import {ConjugationTypes} from "@/lib/constants";
 export const verbSchema = z.object({
     infinitive: z.string().min(1, "Infinitive is required").max(255),
     conjugationType: z.enum(ConjugationTypes),
-    associations: z.string(),
+    associations: z.string().min(1, "Associations are required").max(255, "Associations must be less than 255 characters"),
 });
